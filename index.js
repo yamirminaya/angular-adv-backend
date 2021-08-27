571aca4945af6b7e120776e5ef5914f6b1542d95
Yamir <yamir@moopixel.com>
2021-08-27 00:16:14 -0500

    SPA control

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
+const path = require('path');
+
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+
 const { dbConnection } = require('./db/config');
 
 //* Creando el servidor de express
@@ -25,9 +28,12 @@ app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/upload', require('./routes/uploads'));
 app.use('/api/login', require('./routes/auth'));
-// app.get('/', (req, res) => {
-//   res.json({ ok: true, msg: 'Hola mundo' });
-// });
+
+//* Solución al problema de F5 con ANGULAR
+// Si no encuentras una ruta pasa siempre por INDEX.HTML
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en puerto: ${process.env.PORT}`);
